f52cd3b947a5839b57144e89f3a5272f9d9d0d88
Carlos <caiogottschalk@hotmail.com>
2017-10-04 10:55:41 -0300

    import do projeto

--- src/decaf/DecafParser.g4
+++ src/decaf/DecafParser.g4
@@ -10,4 +10,78 @@ options
   tokenVocab=DecafLexer;
 }
 
-program: TK_class ID LCURLY RCURLY EOF;
+ program:  	     
+           CLASS PROGRAM LCURLY (field_decl)* (method_decl)* RCURLY EOF ;
+ 
+  
+ field_decl:        
+              ( TYPE ID (VIRGULA TYPE ID )?  PONT_V |  TYPE ID  CL INT_LITERAL CR (VIRGULA ID CL INT_LITERAL CR )? PONT_V );
+                 
+ 
+ method_decl:
+              (TYPE | VOID)  ID PL (TYPE ID ( VIRGULA TYPE ID)*)? PR block ; 
+      
+
+
+ block:
+         LCURLY  (var_decl)* (statement)* RCURLY ;
+
+
+ var_decl:
+            TYPE ID (VIRGULA ID)* PONT_V ; 
+	
+ 
+ statement:
+             location (IGUAL_OP|ASSING_OP ) expr PONT_V
+		
+ 		|  method_call PONT_V
+
+		|  IF PL expr PR block (ELSE block)?
+		
+		| FOR  ID IGUAL_OP expr VIRGULA expr
+	
+		| RETURN  (expr)? PONT_V
+
+		| BREAK PONT_V
+
+		| CONTINUE PONT_V
+	
+		| block ;
+	
+
+
+ method_call: 
+               method_name  PL   ( expr (VIRGULA expr)* )? PR 
+
+		| CALLOUT PL STRING_LITERAL   (VIRGULA callout_arg (VIRGULA callout_arg)*)?  PR ;
+
+
+ method_name:
+               ID ;
+
+
+ location: 	  
+            ID 
+
+		|  ID CL expr CR ;
+
+
+ expr: 		
+        location
+		|   method_call
+		|   literal
+                |   expr (BINOP|SMENOS) expr
+		|   SMENOS  expr
+		|   SAFIRMA expr
+		|   PL expr PR ;
+
+ 
+
+callout_arg: 
+              expr | STRING_LITERAL ;
+
+ 
+literal:   
+          CHAR_LITERAL | BOOL_LITERAL | INT_LITERAL;
+
+
--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -2,10 +2,23 @@ package decaf;
 
 import java.io.*;
 //import antlr.Token;
+
+import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+//import org.antlr.v4.runtime.tree.ParseTreeWalker;
+//import org.atlr.v4.runtime.tree.gui.TreeViewer;
+import org.antlr.v4.gui.TreeViewer;
+
 import java6035.tools.CLI.*;
+
+
+
 
 class Main {
     public static void main(String[] args) {
@@ -14,6 +27,7 @@ class Main {
 
         	InputStream inputStream = args.length == 0 ?
                     System.in : new java.io.FileInputStream(CLI.infile);
+
         	if (CLI.target == CLI.SCAN)
         	{
         		DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
@@ -95,24 +109,68 @@ class Main {
 							type = "";
 							break;
 
+
 		        			}
 		        			System.out.println (token.getLine() + type + " " + text);
 		        		}
 		        		done = true;
         			} catch(Exception e) {
+
         	        	// print the error:
         	            System.out.println(CLI.infile+" "+e);
         	            lexer.skip();
-        	        }
+        	        } 
+				
+
+
         		}
+
         	}
-        	else if (CLI.target == CLI.PARSE || CLI.target == CLI.DEFAULT)
+        	  else if (CLI.target == CLI.PARSE || CLI.target == CLI.DEFAULT)
         	{
+        	    // Primeiro faz o parsing da cadeia
         		DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
-				CommonTokenStream tokens = new CommonTokenStream(lexer);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
         		DecafParser parser = new DecafParser(tokens);
-                parser.program();
-        	}
+                        parser.program();
+        	} else if (CLI.target == CLI.INTER) { 
+
+	          DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
+		  CommonTokenStream tokens = new CommonTokenStream(lexer);
+		  DecafParser parser = new DecafParser(tokens);
+
+		// Create Tree View
+		// Source: https://stackoverflow.com/questions/23809005/how-to-display
+		
+                // Adiciona as regras semanticas
+		ParseTree tree = parser.program();
+
+
+                // Se estiver no modo debug imprime a arvore de parsing
+                // Create Tree View
+                // Source: https://stackoverflow.com/questions/23809005/how-to-display-antlr-tree-gui
+
+		//show AST in console
+
+		System.out.println(tree.toStringTree(parser));
+		
+		if(CLI.debug) {
+		//show AST in GUI
+		JFrame frame = new JFrame("Antlr AST");
+		JPanel panel = new JPanel();
+		TreeViewer viewr = new TreeViewer(Arrays.asList(
+		parser.getRuleNames()),tree);
+		viewr.setScale(1.5); //scale a litler
+		panel.add(viewr);
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(200,200);
+		frame.setVisible(true);
+
+	}
+	}
+
+
         	
         } catch(Exception e) {
         	// print the error:
